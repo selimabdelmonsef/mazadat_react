0fe94eff1256a38d84ef9af98ea6ef75f191c2c8
Selim <selimabdelmonsef@gmail.com>
2021-10-09 13:40:10 +0200

    Implementing Custom component for categories

---
+++ src/categories/categories.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import styles from './navbar.module.css';
+import { ButtonToolbar, Nav, Dropdown } from 'rsuite';
+import PropTypes from 'prop-types';
+
+export const MyCategories = ({ titles }) => {
+
+    return (
+        <div className={styles.myNavBar} >
+
+                <ButtonToolbar>
+                    {titles.map((title, index) => {
+                        return <Dropdown className={styles.dropDownTitle} title={title?.label} trigger="hover">
+                            <div>
+                                <Dropdown.Item className={styles.dropDownTitle}>{title?.values}</Dropdown.Item>
+                            </div>
+                        </Dropdown>
+                    })}
+                </ButtonToolbar>
+            </div>
+    );
+}
+MyCategories.propTypes = {
+    titles: PropTypes.array,
+};
+const style ={
+    backgrounColor:"transparent",
+    color:"white"
+}
