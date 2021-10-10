56dc9b91a7db0551a0b6e5f05e4c4ca8ccae51d7
Selim <selimabdelmonsef@gmail.com>
2021-10-10 08:03:54 +0200

    fix web view

---
+++ src/components/priceComponent/priceComponent.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import RangeSlider from '../../filters/range_slider/range_slider';
+import { useDispatch } from 'react-redux';
+import { REDUCERS_CONSTANTS } from '../../constants/reducers.constants';
+import styles from './priceComponent.module.css'
+
+
+const PriceComponent = () => {
+
+
+  const dispatch = useDispatch();
+
+  const setMinMaxPriceRange = (min, max) => {
+    dispatch({
+      type: REDUCERS_CONSTANTS.RANGE.MIN_RANGE,
+      data: min
+    });
+    dispatch({
+      type: REDUCERS_CONSTANTS.RANGE.MAX_RANGE,
+      data: max
+    });
+  };
+
+  return (
+    <div className={styles.rangeSliderComponent}>
+      <h6 className={styles.priceRangeStyle}>PRICES</h6>
+      <RangeSlider
+        min={0}
+        max={1000}
+        onChange={({ min, max }) => setMinMaxPriceRange(min, max)}
+
+      />
+    </div>
+
+
+  );
+}
+export default PriceComponent;
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,36 +1,34 @@
 import React, { useEffect,useState } from 'react';
 import styles from './products.module.css';
-// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { api } from '../../constants/api.constants';
 // import { REDUCERS_CONSTANTS } from '../../constants/reducers.constants';
 import useFetch from '../../fetch/useFetch';
 import PlaceholderLoading from 'react-placeholder-loading';
+import { REDUCERS_CONSTANTS } from '../../constants/reducers.constants';
 
 const Products = () => {
-    const category="";
-    // const category = useSelector(state => state.category);
-    // const productState = useSelector(state => state.products);
-    // const dispatch = useDispatch();
-// const [getData, setData]=useState("");
-    // const { data: products, isPending, error } = useFetch(api.category_products_api.replace("{{categoryName}}", category?.id || 'jewelery'));
-    const { data: products, isPending, error } = useFetch(api.all_products_api);
+    const category = useSelector(state => state.category);
+    const productState = useSelector(state => state.products);
 
+    const dispatch = useDispatch();
+    
+    const { data: products, isPending, error } = useFetch(api.category_products_api.replace("{{categoryName}}", category?.data || 'electronics'));
+
+    const setData = () => {
+        dispatch({
+            type: REDUCERS_CONSTANTS.PRODUCTS.GET_PRODUCTS,
+            data: products
+
+        });
+        dispatch({
+            type: REDUCERS_CONSTANTS.PRODUCTS.UPDATE_PRODUCTS,
+            data: products
+        });
+    }
     useEffect(() => {
-        // setData();
-    }, [products]);
-
-    // const setData = () => {
-        // return products;
-        // dispatch({
-        //     type: REDUCERS_CONSTANTS.PRODUCTS.GET_PRODUCTS,
-        //     data: products
-
-        // })
-        // dispatch({
-        //     type: REDUCERS_CONSTANTS.PRODUCTS.UPDATE_PRODUCTS,
-        //     data: products
-        // })
-    // }
+        setData();
+    },[products]);
 
     return (
         <div className={styles.productsContainer}>
@@ -40,20 +38,22 @@ const Products = () => {
                             width={300}
                             height={20} />}
 
-            {products?.length === 0 ?
+            {productState?.updatedProducts?.length === 0 ?
                 <div className={styles.noProducts}>No Products</div>
                 :
                 <div>
-                    {products && <div className={styles.productsBase}>
-                        {products?.map((element, index) => {
+                    {productState?.updatedProducts && <div className={styles.productsBase}>
+                        {productState?.updatedProducts?.map((element, index) => {
                             return <div className={styles.productsElements}>
-                                <div>
+                        <div className={styles.img_title}>
                                 <img className={styles.imageStyle} src={element?.image} alt="" />
-                                </div>
+   
                                 <div className={styles.productName}>{element?.title}</div>
+                                
                                 <div className={styles.productPrice}>${element?.price}</div>
                                 <div className={styles.productDescription}>{element?.description}</div>
                                 <button className={styles.addToCartBtn}>Add To Cart</button>
+                                </div>
                                 <hr/>
                             </div>
                         })}
